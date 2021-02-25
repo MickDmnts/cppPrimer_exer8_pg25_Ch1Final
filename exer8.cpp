0b203252fefeacf2f1b5700fd55a055b809fa4d7
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-25 17:14:38 +0200

    Exercise completed

--- exer8.cpp
+++ exer8.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include "Sales_item.h"
 
-void BookstoreProgram()
+int BookstoreProgram()
 {
+	std::cout << "Please provide ISBNs as input." << std::endl;
 
+	Sales_item total;
+
+	if (std::cin>>total)
+	{
+		Sales_item trans;
+		while (std::cin>>trans)
+		{
+			if (total.isbn()==trans.isbn())
+			{
+				total += trans;
+			}
+			else
+			{
+				std::cout << "Total: " << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout <<"Total: " << total << std::endl;
+	}
+	else
+	{
+		std::cerr << "No Data!" << std::endl;
+		return -1;
+	}
+	return 0;
 }
 
 int main()
 {
-	BookstoreProgram();
-	system("pause");
-
-	return 0;
+	return BookstoreProgram();
 }
